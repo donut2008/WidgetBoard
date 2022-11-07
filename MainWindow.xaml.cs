052822a91679ed3a085dab9b887f6509168f325b
donut2008 <76439683+donut2008@users.noreply.github.com>
2022-11-07 21:23:46 +0530

    1.0.15 [DEBUG]

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,7 @@
 ﻿// To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Hosting;
-using Microsoft.UI.Xaml.Media;
-using Windows.UI;
-using WidgetBoard.Widgets;
+using Microsoft.UI.Xaml.Controls;
 
 namespace WidgetBoard
 {
@@ -19,13 +15,30 @@ namespace WidgetBoard
 			this.InitializeComponent();
 		}
 
-		private void WidgetGrid_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-        {
-            dbw.Visibility = Visibility.Collapsed;
-            cw.Visibility = Visibility.Visible;
-            aw.Visibility = Visibility.Visible;
-            calw.Visibility = Visibility.Visible;
-            fw.Visibility = Visibility.Visible;
-        }
+		private void NavView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+		{
+			var slPage = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
+			if (args.IsSettingsSelected)
+				ContentFrame.Navigate(typeof(SettingsPage), null);
+			else
+			{
+				switch (slPage.Tag.ToString())
+				{
+					case "WPan":
+						ContentFrame.Navigate(typeof(WidgetsPanel), null);
+						break;
+					case "WSet":
+						ContentFrame.Navigate(typeof(WidgetsSettings), null);
+						break;
+					default:
+						ContentDialog NavFailure = new ContentDialog()
+						{
+							Title = "Navigation failed",
+							Content = "Navigation failed due to internal error."
+						};
+						break;
+				}
+			}
+		}
 	}
 }
