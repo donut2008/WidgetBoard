c5c97384fb1512c84761fdba1a5e42878f67a309
donut2008 <76439683+donut2008@users.noreply.github.com>
2022-11-07 21:35:58 +0530

    Version 1.0.23 [DEBUG]

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,13 @@
 ﻿// To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+using Microsoft.UI.Xaml;
+using Microsoft.UI;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI.ViewManagement;
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
 
 namespace WidgetBoard
 {
@@ -13,6 +19,7 @@ namespace WidgetBoard
         public MainWindow()
 		{
 			this.InitializeComponent();
+			SetCustomTitleBar();
 		}
 
 		private void NavView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
@@ -38,6 +45,41 @@ namespace WidgetBoard
 						};
 						break;
 				}
+			}
+		}
+		UISettings UISettings = new UISettings();
+		private void SetCustomTitleBar()
+		{
+			var RootUI = (FrameworkElement)Content;
+			if (AppWindowTitleBar.IsCustomizationSupported())
+			{
+				AppWindow appWindow =
+					AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(WindowNative.GetWindowHandle(this)));
+				var titleBar = appWindow.TitleBar;
+				titleBar.ExtendsContentIntoTitleBar = true;
+
+				void SetColor()
+				{
+					appWindow.Title = "MCInstaller";
+					titleBar.ExtendsContentIntoTitleBar = true;
+					titleBar.ButtonBackgroundColor = Colors.Transparent;
+					titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+					titleBar.ButtonInactiveForegroundColor = UISettings.GetColorValue(UIColorType.Accent);
+					titleBar.ButtonHoverBackgroundColor =
+						((SolidColorBrush)App.Current.Resources.ThemeDictionaries[
+							"SystemControlBackgroundListLowBrush"]).Color;
+					titleBar.ButtonPressedBackgroundColor =
+						((SolidColorBrush)App.Current.Resources.ThemeDictionaries[
+							"SystemControlBackgroundListMediumBrush"]).Color;
+				}
+
+				RootUI.ActualThemeChanged += (_, _) => SetColor();
+				SetColor();
+			}
+			else
+			{
+				ExtendsContentIntoTitleBar = true;
+				SetTitleBar(AppTitleBar);
 			}
 		}
 	}
